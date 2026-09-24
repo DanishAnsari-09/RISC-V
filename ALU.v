d9035fbf8acdf6d74ebd68bde164f47725d17868
DanishAnsari-09 <mohammeddanishansari09@gmail.com>
2026-09-24 15:39:55 +0530

    updated ALU

--- ALU.v
+++ ALU.v
@@ -1,4 +1,32 @@
-module alu(A, B, ALU_Sel, ALU_Out, Zero, Negative, Carry, Overflow);
+module ALU(A,B,Result,ALUControl,OverFlow,Carry,Zero,Negative);
+
+    input [31:0]A,B;
+    input [2:0]ALUControl;
+    output Carry,OverFlow,Zero,Negative;
+    output [31:0]Result;
+
+    wire Cout;
+    wire [31:0]Sum;
+
+    assign {Cout,Sum} = (ALUControl[0] == 1'b0) ? A + B :
+                                          (A + ((~B)+1)) ;
+    assign Result = (ALUControl == 3'b000) ? Sum :
+                    (ALUControl == 3'b001) ? Sum :
+                    (ALUControl == 3'b010) ? A & B :
+                    (ALUControl == 3'b011) ? A | B :
+                    (ALUControl == 3'b101) ? {{31{1'b0}},(Sum[31])} : {32{1'b0}};
+    
+    assign OverFlow = ((Sum[31] ^ A[31]) & 
+                      (~(ALUControl[0] ^ B[31] ^ A[31])) &
+                      (~ALUControl[1]));
+    assign Carry = ((~ALUControl[1]) & Cout);
+    assign Zero = &(~Result);
+    assign Negative = Result[31];
+
+endmodule
+
+
+/*module alu(A, B, ALU_Sel, ALU_Out, Zero, Negative, Carry, Overflow);
   input [31:0] A, B;
   input [2:0] ALU_Sel;
   output reg [31:0] ALU_Out;
@@ -25,3 +53,4 @@ module alu(A, B, ALU_Sel, ALU_Out, Zero, Negative, Carry, Overflow);
   assign Overflow = 1'b0; // Placeholder - actual implementation depends on the specific operation
 
 endmodule;
+*/
